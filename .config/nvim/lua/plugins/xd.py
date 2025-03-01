9bde37622de6db98e0d4918bed58a837deba2509
elo <rmandanehvash@gmail.com>
2025-03-01 10:03:59 +0330

    fixed nvim config for codeium giving erros

--- .config/nvim/lua/plugins/xd.py
+++
@@ -1,9 +0,0 @@
-for i in range (1, 100): 
-    print("hello")
-    print("world")
-    if i % 10 == 0:
-        print("ten")
-    if i % 50 == 0:
-        print("fifty")
-    if i % 100 == 0:
-        print("hundred")
